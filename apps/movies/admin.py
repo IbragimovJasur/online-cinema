cfbc22d8bcc5e6460c9a9feeb3a59d6289ad63a3
IbragimovJasur <ij.ilkhambekovich@gmail.com>
2021-10-03 12:14:56 +0500

    changes in admin of movies app

--- apps/movies/admin.py
+++ apps/movies/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Genre, Movie, Rating
 
-models_to_register= [Genre, Movie, Rating]
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display= ['name', 'duration', 'type']
+    list_display_links= ['name']
 
-for one_model in models_to_register:
-    admin.site.register(one_model)
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display= ['movie', 'rating_of_film', ]
+    list_display_links= ['movie']
+
+admin.site.register(Genre)
     
